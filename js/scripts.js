5d1534d7dd5d882a37262e3cb051df06bc1fc07e
Brittany Rausch and John Klein <brittany@>
2016-03-23 09:25:59 -0700

    Create a story based on the users input

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var adjectiveInput = $("input#adjective").val();
+    var activityInput = $("input#activity").val();
+    var exclamationInput = $("input#exclamation").val();
+    var emotionInput = $("input#emotion").val();
+
+    $(".person1").text(person1Input);
+    $(".adjective").text(adjectiveInput);
+    $(".activity").text(activityInput);
+    $(".exclamation").text(exclamationInput);
+    $(".emotion").text(emotionInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
